b2611c7f75cf73740b5e9b1b54e2147f3659d41e
Mario Gómez <mariogomezmtz@gmail.com>
2021-08-11 17:29:22 -0500

    Add user and admin from devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,26 @@
 
 Rails.application.routes.draw do
   root to: "application#index"
+
+  devise_for :admins, path: "admin", skip: [:registrations], controllers: {
+    sessions: "admins/sessions",
+    passwords: "admins/passwords",
+  }
+
+  devise_for :users, path: "user", skip: [:registrations], controllers: {
+    sessions: "users/sessions",
+    passwords: "users/passwords",
+  }
+
+  # Disable new registrations
+  as :admin do
+    get "admin/edit" => "admins/registrations#edit", as: "edit_admin_registration"
+    put "admin" => "admins/registrations#update", as: "admin_registration"
+  end
+
+  as :user do
+    get "user/edit" => "users/registrations#edit", as: "edit_user_registration"
+    put "user" => "users/registrations#update", as: "user_registration"
+  end
+
 end
