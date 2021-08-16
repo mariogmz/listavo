7a43572ef18999be0d2e873da90d41237d203ca0
Mario Gómez <mariogomezmtz@gmail.com>
2021-08-16 00:20:42 -0500

    Add bootstrap builder

---
+++ app/builders/bootstrap_builder.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+class BootstrapBuilder < ActionView::Helpers::FormBuilder
+  def label(method, text = nil, options = {}, &block)
+    super(method, text, options.reverse_merge(class: "form-label"), &block)
+  end
+
+  def email_field(method, options = {})
+    super(method, options.reverse_merge(class: "form-control"))
+  end
+
+  def password_field(method, options = {})
+    super(method, options.reverse_merge(class: "form-control"))
+  end
+
+  def text_field(method, options = {})
+    super(method, options.reverse_merge(class: "form-control"))
+  end
+
+  def telephone_field(object_name, method, options = {})
+    super(object_name, method, options.reverse_merge(class: "form-control"))
+  end
+
+  def number_field(object_name, method, options = {})
+    super(object_name, method, options.reverse_merge(class: "form-control"))
+  end
+
+  def submit(value = nil, options = {})
+    super(value, options.reverse_merge(class: "btn btn-primary"))
+  end
+
+  def check_box(method, options = {}, checked_value = "1", unchecked_value = "0")
+    super(method, options.reverse_merge(class: "form-check-input"), checked_value, unchecked_value)
+  end
+
+  def select(method, choices = nil, options = {}, html_options = {}, &block)
+    super(method, choices, options, html_options.reverse_merge(class: "form-select"), &block)
+  end
+end
